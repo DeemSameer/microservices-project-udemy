129fbcfb80c4d34ed23b91f72590d14ec1125c8c
deem_sameer <66889295+DeemSameer@users.noreply.github.com>
2025-03-13 12:03:33 +0300

    ssl

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -7,6 +7,7 @@ export const config = {
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
+  'ssl': process.env.SSL,
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
